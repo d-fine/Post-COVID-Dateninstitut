f50ea5138e86404c41c381ed85409136fa8dfdd5
Martin Bercx <Martin.Bercx@d-fine.com>
2025-05-02 17:34:15 +0200

    Remove code

--- datenmodell-backend/src/main/kotlin/org/datenmodell/datenmodellbackend/repository/StudyRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.datenmodell.datenmodellbackend.repository
-
-import org.datenmodell.datenmodellbackend.entities.StudyEntity
-import org.springframework.data.repository.CrudRepository
-import java.util.UUID
-
-interface StudyRepository : CrudRepository<StudyEntity, UUID>
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "datenmodell"
-include("datenmodell-backend")
-include("clientapp-backend")
